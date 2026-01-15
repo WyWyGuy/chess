fc88104cecfe09f5d39bd84c62b86c2cbb87ccb6
WyWyGuy <wywywyguy@gmail.com>
2026-01-15 16:41:54 -0700

    Implemented BishopMoves.java. Verified with tests.

--- shared/src/main/java/chess/BishopMoves.java
+++ shared/src/main/java/chess/BishopMoves.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class BishopMoves {
@@ -13,7 +14,88 @@ public class BishopMoves {
     }
 
     public Collection<ChessMove> GenerateMoves() {
-        throw new RuntimeException("Not implemented");
+        ArrayList<ChessMove> possibleMoves = new ArrayList<ChessMove>();
+        if ((this.position.getRow() < 8) && (this.position.getColumn() < 8)) {
+            int newRow = this.position.getRow() + 1;
+            int newCol = this.position.getColumn() + 1;
+            ChessPosition newPos = new ChessPosition(newRow, newCol);
+            ChessPiece pieceAlreadyThere = this.board.getPiece(newPos);
+            while ((newRow <= 8) && (newCol <= 8)) {
+                pieceAlreadyThere = this.board.getPiece(newPos);
+                if ((pieceAlreadyThere != null) && (pieceAlreadyThere.getTeamColor() == board.getPiece(this.position).getTeamColor())) {
+                    break;
+                }
+                ChessMove possibleMove = new ChessMove(this.position, newPos, null);
+                possibleMoves.add(possibleMove);
+                if (pieceAlreadyThere != null) {
+                    break;
+                }
+                newRow++;
+                newCol++;
+                newPos = new ChessPosition(newRow, newCol);
+            }
+        }
+        if ((this.position.getRow() < 8) && (this.position.getColumn() > 1)) {
+            int newRow = this.position.getRow() + 1;
+            int newCol = this.position.getColumn() - 1;
+            ChessPosition newPos = new ChessPosition(newRow, newCol);
+            ChessPiece pieceAlreadyThere = this.board.getPiece(newPos);
+            while ((newRow <= 8) && (newCol >= 1)) {
+                pieceAlreadyThere = this.board.getPiece(newPos);
+                if ((pieceAlreadyThere != null) && (pieceAlreadyThere.getTeamColor() == board.getPiece(this.position).getTeamColor())) {
+                    break;
+                }
+                ChessMove possibleMove = new ChessMove(this.position, newPos, null);
+                possibleMoves.add(possibleMove);
+                if (pieceAlreadyThere != null) {
+                    break;
+                }
+                newRow++;
+                newCol--;
+                newPos = new ChessPosition(newRow, newCol);
+            }
+        }
+        if ((this.position.getRow() > 1) && (this.position.getColumn() < 8)) {
+            int newRow = this.position.getRow() - 1;
+            int newCol = this.position.getColumn() + 1;
+            ChessPosition newPos = new ChessPosition(newRow, newCol);
+            ChessPiece pieceAlreadyThere = this.board.getPiece(newPos);
+            while ((newRow >= 1) && (newCol <= 8)) {
+                pieceAlreadyThere = this.board.getPiece(newPos);
+                if ((pieceAlreadyThere != null) && (pieceAlreadyThere.getTeamColor() == board.getPiece(this.position).getTeamColor())) {
+                    break;
+                }
+                ChessMove possibleMove = new ChessMove(this.position, newPos, null);
+                possibleMoves.add(possibleMove);
+                if (pieceAlreadyThere != null) {
+                    break;
+                }
+                newRow--;
+                newCol++;
+                newPos = new ChessPosition(newRow, newCol);
+            }
+        }
+        if ((this.position.getRow() > 1) && (this.position.getColumn() > 1)) {
+            int newRow = this.position.getRow() - 1;
+            int newCol = this.position.getColumn() - 1;
+            ChessPosition newPos = new ChessPosition(newRow, newCol);
+            ChessPiece pieceAlreadyThere = this.board.getPiece(newPos);
+            while ((newRow >= 1) && (newCol >= 1)) {
+                pieceAlreadyThere = this.board.getPiece(newPos);
+                if ((pieceAlreadyThere != null) && (pieceAlreadyThere.getTeamColor() == board.getPiece(this.position).getTeamColor())) {
+                    break;
+                }
+                ChessMove possibleMove = new ChessMove(this.position, newPos, null);
+                possibleMoves.add(possibleMove);
+                if (pieceAlreadyThere != null) {
+                    break;
+                }
+                newRow--;
+                newCol--;
+                newPos = new ChessPosition(newRow, newCol);
+            }
+        }
+        return possibleMoves;
     }
 
 }
