20788d073d73da0a0834700a2662781ebc4037e7
WyWyGuy <wywywyguy@gmail.com>
2026-01-15 20:16:09 -0700

    Implemented PawnMoves.java. Verified with tests.

--- shared/src/main/java/chess/PawnMoves.java
+++ shared/src/main/java/chess/PawnMoves.java
@@ -15,7 +15,141 @@ public class PawnMoves {
 
     public Collection<ChessMove> GenerateMoves() {
         ArrayList<ChessMove> possibleMoves = new ArrayList<ChessMove>();
-
+        ChessPiece thisPawn = this.board.getPiece(this.position);
+        ChessGame.TeamColor thisPawnColor = thisPawn.getTeamColor();
+        if (thisPawnColor == ChessGame.TeamColor.WHITE) {
+            if (this.position.getRow() < 8) {
+                ChessPosition forwardByOne = new ChessPosition(this.position.getRow() + 1, this.position.getColumn());
+                ChessPiece pieceDirectlyInFront = this.board.getPiece(forwardByOne);
+                if (pieceDirectlyInFront == null) {
+                    if (forwardByOne.getRow() < 8) {
+                        ChessMove possibleMove = new ChessMove(this.position, forwardByOne, null);
+                        possibleMoves.add(possibleMove);
+                    } else if (forwardByOne.getRow() == 8) {
+                        ChessMove possibleQueenPromotion = new ChessMove(this.position, forwardByOne, ChessPiece.PieceType.QUEEN);
+                        possibleMoves.add(possibleQueenPromotion);
+                        ChessMove possibleRookPromotion = new ChessMove(this.position, forwardByOne, ChessPiece.PieceType.ROOK);
+                        possibleMoves.add(possibleRookPromotion);
+                        ChessMove possibleBishopPromotion = new ChessMove(this.position, forwardByOne, ChessPiece.PieceType.BISHOP);
+                        possibleMoves.add(possibleBishopPromotion);
+                        ChessMove possibleKnightPromotion = new ChessMove(this.position, forwardByOne, ChessPiece.PieceType.KNIGHT);
+                        possibleMoves.add(possibleKnightPromotion);
+                    }
+                    if (this.position.getRow() == 2) {
+                        ChessPosition forwardByTwo = new ChessPosition(this.position.getRow() + 2, this.position.getColumn());
+                        ChessPiece pieceTwoInFront = this.board.getPiece(forwardByTwo);
+                        if (pieceTwoInFront == null) {
+                            ChessMove possibleMoveTwo = new ChessMove(this.position, forwardByTwo, null);
+                            possibleMoves.add(possibleMoveTwo);
+                        }
+                    }
+                }
+                if (this.position.getColumn() > 1) {
+                    ChessPosition potentialLeftCapture = new ChessPosition(this.position.getRow() + 1, this.position.getColumn() - 1);
+                    ChessPiece potentialLeftCapturePiece = this.board.getPiece(potentialLeftCapture);
+                    if ((potentialLeftCapturePiece != null) && (potentialLeftCapturePiece.getTeamColor() != this.board.getPiece(this.position).getTeamColor())) {
+                        if (potentialLeftCapture.getRow() < 8) {
+                            ChessMove possibleLeftCapture = new ChessMove(this.position, potentialLeftCapture, null);
+                            possibleMoves.add(possibleLeftCapture);
+                        } else if (potentialLeftCapture.getRow() == 8) {
+                            ChessMove possibleCaptureQueenPromotion = new ChessMove(this.position, potentialLeftCapture, ChessPiece.PieceType.QUEEN);
+                            possibleMoves.add(possibleCaptureQueenPromotion);
+                            ChessMove possibleCaptureRookPromotion = new ChessMove(this.position, potentialLeftCapture, ChessPiece.PieceType.ROOK);
+                            possibleMoves.add(possibleCaptureRookPromotion);
+                            ChessMove possibleCaptureBishopPromotion = new ChessMove(this.position, potentialLeftCapture, ChessPiece.PieceType.BISHOP);
+                            possibleMoves.add(possibleCaptureBishopPromotion);
+                            ChessMove possibleCaptureKnightPromotion = new ChessMove(this.position, potentialLeftCapture, ChessPiece.PieceType.KNIGHT);
+                            possibleMoves.add(possibleCaptureKnightPromotion);
+                        }
+                    }
+                }
+                if (this.position.getColumn() < 8) {
+                    ChessPosition potentialRightCapture = new ChessPosition(this.position.getRow() + 1, this.position.getColumn() + 1);
+                    ChessPiece potentialRightCapturePiece = this.board.getPiece(potentialRightCapture);
+                    if ((potentialRightCapturePiece != null) && (potentialRightCapturePiece.getTeamColor() != this.board.getPiece(this.position).getTeamColor())) {
+                        if (potentialRightCapture.getRow() < 8) {
+                            ChessMove possibleRightCapture = new ChessMove(this.position, potentialRightCapture, null);
+                            possibleMoves.add(possibleRightCapture);
+                        } else if (potentialRightCapture.getRow() == 8) {
+                            ChessMove possibleCaptureQueenPromotion = new ChessMove(this.position, potentialRightCapture, ChessPiece.PieceType.QUEEN);
+                            possibleMoves.add(possibleCaptureQueenPromotion);
+                            ChessMove possibleCaptureRookPromotion = new ChessMove(this.position, potentialRightCapture, ChessPiece.PieceType.ROOK);
+                            possibleMoves.add(possibleCaptureRookPromotion);
+                            ChessMove possibleCaptureBishopPromotion = new ChessMove(this.position, potentialRightCapture, ChessPiece.PieceType.BISHOP);
+                            possibleMoves.add(possibleCaptureBishopPromotion);
+                            ChessMove possibleCaptureKnightPromotion = new ChessMove(this.position, potentialRightCapture, ChessPiece.PieceType.KNIGHT);
+                            possibleMoves.add(possibleCaptureKnightPromotion);
+                        }
+                    }
+                }
+            }
+        } else if (thisPawnColor == ChessGame.TeamColor.BLACK) {
+            if (this.position.getRow() < 8) {
+                ChessPosition forwardByOne = new ChessPosition(this.position.getRow() - 1, this.position.getColumn());
+                ChessPiece pieceDirectlyInFront = this.board.getPiece(forwardByOne);
+                if (pieceDirectlyInFront == null) {
+                    if (forwardByOne.getRow() > 1) {
+                        ChessMove possibleMove = new ChessMove(this.position, forwardByOne, null);
+                        possibleMoves.add(possibleMove);
+                    } else if (forwardByOne.getRow() == 1) {
+                        ChessMove possibleQueenPromotion = new ChessMove(this.position, forwardByOne, ChessPiece.PieceType.QUEEN);
+                        possibleMoves.add(possibleQueenPromotion);
+                        ChessMove possibleRookPromotion = new ChessMove(this.position, forwardByOne, ChessPiece.PieceType.ROOK);
+                        possibleMoves.add(possibleRookPromotion);
+                        ChessMove possibleBishopPromotion = new ChessMove(this.position, forwardByOne, ChessPiece.PieceType.BISHOP);
+                        possibleMoves.add(possibleBishopPromotion);
+                        ChessMove possibleKnightPromotion = new ChessMove(this.position, forwardByOne, ChessPiece.PieceType.KNIGHT);
+                        possibleMoves.add(possibleKnightPromotion);
+                    }
+                    if (this.position.getRow() == 7) {
+                        ChessPosition forwardByTwo = new ChessPosition(this.position.getRow() - 2, this.position.getColumn());
+                        ChessPiece pieceTwoInFront = this.board.getPiece(forwardByTwo);
+                        if (pieceTwoInFront == null) {
+                            ChessMove possibleMoveTwo = new ChessMove(this.position, forwardByTwo, null);
+                            possibleMoves.add(possibleMoveTwo);
+                        }
+                    }
+                }
+                if (this.position.getColumn() > 1) {
+                    ChessPosition potentialLeftCapture = new ChessPosition(this.position.getRow() - 1, this.position.getColumn() - 1);
+                    ChessPiece potentialLeftCapturePiece = this.board.getPiece(potentialLeftCapture);
+                    if ((potentialLeftCapturePiece != null) && (potentialLeftCapturePiece.getTeamColor() != this.board.getPiece(this.position).getTeamColor())) {
+                        if (potentialLeftCapture.getRow() > 1) {
+                            ChessMove possibleLeftCapture = new ChessMove(this.position, potentialLeftCapture, null);
+                            possibleMoves.add(possibleLeftCapture);
+                        } else if (potentialLeftCapture.getRow() == 1) {
+                            ChessMove possibleCaptureQueenPromotion = new ChessMove(this.position, potentialLeftCapture, ChessPiece.PieceType.QUEEN);
+                            possibleMoves.add(possibleCaptureQueenPromotion);
+                            ChessMove possibleCaptureRookPromotion = new ChessMove(this.position, potentialLeftCapture, ChessPiece.PieceType.ROOK);
+                            possibleMoves.add(possibleCaptureRookPromotion);
+                            ChessMove possibleCaptureBishopPromotion = new ChessMove(this.position, potentialLeftCapture, ChessPiece.PieceType.BISHOP);
+                            possibleMoves.add(possibleCaptureBishopPromotion);
+                            ChessMove possibleCaptureKnightPromotion = new ChessMove(this.position, potentialLeftCapture, ChessPiece.PieceType.KNIGHT);
+                            possibleMoves.add(possibleCaptureKnightPromotion);
+                        }
+                    }
+                }
+                if (this.position.getColumn() < 8) {
+                    ChessPosition potentialRightCapture = new ChessPosition(this.position.getRow() - 1, this.position.getColumn() + 1);
+                    ChessPiece potentialRightCapturePiece = this.board.getPiece(potentialRightCapture);
+                    if ((potentialRightCapturePiece != null) && (potentialRightCapturePiece.getTeamColor() != this.board.getPiece(this.position).getTeamColor())) {
+                        if (potentialRightCapture.getRow() > 1) {
+                            ChessMove possibleRightCapture = new ChessMove(this.position, potentialRightCapture, null);
+                            possibleMoves.add(possibleRightCapture);
+                        } else if (potentialRightCapture.getRow() == 1) {
+                            ChessMove possibleCaptureQueenPromotion = new ChessMove(this.position, potentialRightCapture, ChessPiece.PieceType.QUEEN);
+                            possibleMoves.add(possibleCaptureQueenPromotion);
+                            ChessMove possibleCaptureRookPromotion = new ChessMove(this.position, potentialRightCapture, ChessPiece.PieceType.ROOK);
+                            possibleMoves.add(possibleCaptureRookPromotion);
+                            ChessMove possibleCaptureBishopPromotion = new ChessMove(this.position, potentialRightCapture, ChessPiece.PieceType.BISHOP);
+                            possibleMoves.add(possibleCaptureBishopPromotion);
+                            ChessMove possibleCaptureKnightPromotion = new ChessMove(this.position, potentialRightCapture, ChessPiece.PieceType.KNIGHT);
+                            possibleMoves.add(possibleCaptureKnightPromotion);
+                        }
+                    }
+                }
+            }
+        }
         return possibleMoves;
     }
 
