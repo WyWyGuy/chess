0f8ca06665648008774d60443b0d436e9bf2b3e9
WyWyGuy <wywywyguy@gmail.com>
2026-01-28 23:39:07 -0700

    Added variables for castling and en passant later. Implemented constructor, getter/setter. Implement isInCheck and created a helper function called getKingPosition.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -10,15 +11,34 @@ import java.util.Collection;
  */
 public class ChessGame {
 
-    public ChessGame() {
+    private TeamColor currentTurn;
+    private ChessBoard board;
+    private boolean whiteLeftRookHasMoved;
+    private boolean whiteRightRookHasMoved;
+    private boolean whiteKingHasMoved;
+    private boolean blackLeftRookHasMoved;
+    private boolean blackRightRookHasMoved;
+    private boolean blackKingHasMoved;
+    private ChessPosition isEnPassantable;
 
+    public ChessGame() {
+        this.currentTurn = TeamColor.WHITE;
+        this.board = new ChessBoard();
+        this.board.resetBoard();
+        this.whiteLeftRookHasMoved = false;
+        this.whiteRightRookHasMoved = false;
+        this.whiteKingHasMoved = false;
+        this.blackLeftRookHasMoved = false;
+        this.blackRightRookHasMoved = false;
+        this.blackKingHasMoved = false;
+        this.isEnPassantable = null;
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return this.currentTurn;
     }
 
     /**
@@ -27,7 +47,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.currentTurn = team;
     }
 
     /**
@@ -38,6 +58,19 @@ public class ChessGame {
         BLACK
     }
 
+    public ChessPosition getKingPosition(TeamColor color) throws NoKingFoundException {
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                ChessPosition ijPosition = new ChessPosition(i, j);
+                ChessPiece ijPiece = this.board.getPiece(ijPosition);
+                if (ijPiece != null && ijPiece.getTeamColor() == color && ijPiece.getPieceType() == ChessPiece.PieceType.KING) {
+                    return ijPosition;
+                }
+            }
+        }
+        throw new NoKingFoundException(String.format("%s king not found on the board.", color));
+    }
+
     /**
      * Gets a valid moves for a piece at the given location
      *
@@ -46,7 +79,13 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece pieceThere = this.board.getPiece(startPosition);
+        if (pieceThere == null) {
+            return null;
+        } else {
+            //Need some more logic here
+            return pieceThere.pieceMoves(this.board, startPosition);
+        }
     }
 
     /**
@@ -56,7 +95,21 @@ public class ChessGame {
      * @throws InvalidMoveException if move is invalid
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
-        throw new RuntimeException("Not implemented");
+        ChessPiece movingPiece = this.board.getPiece(move.getStartPosition());
+        Collection<ChessMove> validMoves = this.validMoves(move.getStartPosition());
+        if (validMoves != null && validMoves.contains(move)) {
+            this.board.addPiece(move.getEndPosition(), movingPiece);
+            this.board.addPiece(move.getStartPosition(), null);
+            //If castling, move rook too. (test by "king moves for the first time and it's two?")
+            //If enPassanting, delete pawn too. (test by "pawn captures behind enPassantable")
+        } else {
+            throw new InvalidMoveException(String.format("Cannot move %s from row %d, col %d to row %d, col %d.",
+                    (movingPiece == null ? null : movingPiece.getPieceType()),
+                    move.getStartPosition().getRow(),
+                    move.getStartPosition().getColumn(),
+                    move.getEndPosition().getRow(),
+                    move.getEndPosition().getColumn()));
+        }
     }
 
     /**
@@ -66,7 +119,27 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        ChessPosition kingPosition;
+        try {
+            kingPosition = this.getKingPosition(teamColor);
+        } catch (NoKingFoundException e) {
+            return false;
+        }
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                ChessPosition ijPosition = new ChessPosition(i, j);
+                ChessPiece ijPiece = this.board.getPiece(ijPosition);
+                if (ijPiece != null && ijPiece.getTeamColor() != teamColor) {
+                    Collection<ChessMove> ijMoves = ijPiece.pieceMoves(this.board, ijPosition);
+                    for (ChessMove move : ijMoves) {
+                        if (move.getEndPosition().equals(kingPosition)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     /**
@@ -76,7 +149,13 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        ChessPosition kingPosition;
+        try {
+            kingPosition = this.getKingPosition(teamColor);
+        } catch (NoKingFoundException e) {
+            return false;
+        }
+        return false; //Need more logic
     }
 
     /**
@@ -87,7 +166,9 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return false;
+        //Needs more logic
+        //Not in check, All pieces moves combined are zero moves.
     }
 
     /**
@@ -96,7 +177,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -105,6 +186,37 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return this.board;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return whiteLeftRookHasMoved == chessGame.whiteLeftRookHasMoved &&
+                whiteRightRookHasMoved == chessGame.whiteRightRookHasMoved &&
+                whiteKingHasMoved == chessGame.whiteKingHasMoved &&
+                blackLeftRookHasMoved == chessGame.blackLeftRookHasMoved &&
+                blackRightRookHasMoved == chessGame.blackRightRookHasMoved &&
+                blackKingHasMoved == chessGame.blackKingHasMoved &&
+                currentTurn == chessGame.currentTurn &&
+                Objects.equals(board, chessGame.board) &&
+                Objects.equals(isEnPassantable, chessGame.isEnPassantable);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                currentTurn,
+                board,
+                whiteLeftRookHasMoved,
+                whiteRightRookHasMoved,
+                whiteKingHasMoved,
+                blackLeftRookHasMoved,
+                blackRightRookHasMoved,
+                blackKingHasMoved,
+                isEnPassantable);
     }
 }
---
+++ shared/src/main/java/chess/NoKingFoundException.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class NoKingFoundException extends Exception {
+
+    public NoKingFoundException() {}
+
+    public NoKingFoundException(String message) {
+        super(message);
+    }
+}
