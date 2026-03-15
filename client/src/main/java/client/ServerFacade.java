ca62fea5e56c21c5d3f220031a452ca8b479e73e
WyWyGuy <wywywyguy@gmail.com>
2026-03-15 15:11:23 -0600

    Created ServerFacade.java.

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,32 @@
+package client;
+
+import model.AuthData;
+import model.GameData;
+
+public class ServerFacade {
+
+    public AuthData register() {
+
+    }
+
+    public AuthData login() {
+
+    }
+
+    public void logout() {
+
+    }
+
+    public int createGame() {
+
+    }
+
+    public Container<GameData> listGames() {
+
+    }
+
+    public void joinGame() {
+
+    }
+    
+}
